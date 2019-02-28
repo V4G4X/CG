e7a30f638bf62f75abfaf109d676bc844a3fd1ad
V4G4X <varun.gawande@gmail.com>
2019-02-28 10:22:04 +0530

    modified:   Mouse.c

--- Mouse.c
+++ Mouse.c
@@ -3,38 +3,76 @@
 #include<GL/gl.h>
 #include<math.h>
 
-void myInit(void)
-{
-		
-	glClearColor(1.0, 1.0, 1.0, 0.0);
+int event;
+
+void myInit(void){
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glColor3f(0.0f, 0.0f, 1.0f);
-	gluOrtho2D(0,640,480,0);
+	gluOrtho2D(0,800,450,0);
+	
 }
 
-void display()
-{
-	printf("\n2\n");
+void initrect(){
+	//Rectangle 1	RED
+	glBegin(GL_POLYGON);
+	glColor3f(1.0,0.0,0.0);
+	glVertex2i(0,0);
+	glVertex2i(50,0);
+	glVertex2i(50,50);
+	glVertex2i(0,50);
+	glEnd();
+	//Rectangle 2	GREEN
+	glBegin(GL_POLYGON);
+	glColor3f(0.0,1.0,0.0);
+	glVertex2i(60,0);
+	glVertex2i(110,0);
+	glVertex2i(110,50);
+	glVertex2i(60,50);
+	glEnd();
+	//Rectangle 3	BLUE
+	glBegin(GL_POLYGON);
+	glColor3f(0.0,0.0,1.0);
+	glVertex2i(120,0);
+	glVertex2i(170,0);
+	glVertex2i(170,50);
+	glVertex2i(120,50);
+	glEnd();
+}
+
+void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
+	initrect();
 	glFlush();
 }
 
-void mouseDemo(int btn,int st,int x,int y)
-{
-	printf("\n%d  %d\n",x,y);
+void mouseDemo(int btn,int st,int x,int y){
+	event++;
+	if(event%2==0){
+		printf("\nEvent:%d\n",(event/2));
+		printf("x:%d\ty:%d\n",x,y);
+		printf("btn:%d\tst:%d\n",btn,st);
+	}
 	static int xst,yst,pt=0;
-	if(btn==GLUT_LEFT_BUTTON&&st==GLUT_DOWN)
-	{
-		if(pt==0)
-		{
+	//Change colour incase of palette event
+	//Red Box
+	if((x>0 && x<50)&& (y>0 && y<50))
+		glColor3f(1.0,0.0,0.0);
+	//Green Box
+	else if((x>60 && x<110)&& (y>0 && y<50))
+		glColor3f(0.0,1.0,0.0);
+	//Blue Box
+	else if((x>120 && x<170)&& (y>0 && y<50))
+		glColor3f(0.0,0.0,1.0);
+	
+	else if(btn==GLUT_LEFT_BUTTON&&st==GLUT_DOWN){
+		if(pt==0){
 			xst=x;
 			yst=y;
 			pt++;
-			printf("\n3\n");		
+		//	glColor3f(1.0f, 0.0f, 1.0f);
 		}
 		else{
 			glBegin(GL_LINES);
-			glColor3f(1.0f, 0.0f, 1.0f);
-			printf("\n4\n");			
 			glVertex2i(xst,yst);
 			glVertex2i(x,y);
 			glEnd();
@@ -42,23 +80,32 @@ void mouseDemo(int btn,int st,int x,int y)
 			yst=y;
 		}
 	}
-	else if(btn==GLUT_RIGHT_BUTTON&&st==GLUT_DOWN)
-	{
+	else if(btn==GLUT_RIGHT_BUTTON&&st==GLUT_DOWN){
 		pt=0;
+	}
+	else if(btn==GLUT_MIDDLE_BUTTON && st==GLUT_DOWN){
+		static int c=0;
+		if(c==0){
+			glColor3f(0.0f, 1.0f, 1.0f);
+			c=1;
+		}
+		else{
+			glColor3f(1.0f, 0.0f, 1.0f);
+			c=0;
+		}
 	}
 glFlush();	
 }
-int main(int argc, char** argv)
-{
+
+int main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(750,750);
+	glutInitWindowSize(800,450);
 	glutInitWindowPosition(0,0);
-	glutCreateWindow("Tom & Jerry");
-	printf("\n1\n");
+	glutCreateWindow("Testing Mouse");
+	myInit();
 	glutDisplayFunc(display);
 	glutMouseFunc(mouseDemo);
-	myInit();
 	glutMainLoop();
 	return 0;
 }
